c4f6ca0cba70b7de516642eb0161bf5565956341
mijanrony12e <mijaninfo12@gmail.comm>
2021-09-02 04:06:27 +0600

    HTML and js

--- book.js
+++ book.js
@@ -1,1 +1,73 @@
-alert('hi')
+const error = document.getElementById('error');
+error.style.display = 'none';
+// handler add with button.
+document.getElementById('button').addEventListener('click', (e) => {
+    e.preventDefault()
+
+    //access input feild
+    const inputField = document.getElementById('input-field');
+    const inputValue = inputField.value;
+    inputField.value = '';
+
+    //check input feild length
+    if (inputValue.length >= 1)
+    {
+        receiveInputValue(inputValue);
+    } else
+    {
+        alert('give me some text')
+    }
+});
+
+// doing Api load
+const receiveInputValue = async bookName => {
+    const url = `http://openlibrary.org/search.json?q=${bookName}`;
+    try
+    {
+        const res = await fetch(url)
+        const data= await res.json()
+        displayData(data.docs);
+    } catch (err)
+    {
+       error.style.display = 'block';
+    }
+}
+//display data on HTML Page.
+const displayData = (book) => {
+    console.log(book.length);
+    if (book.length === 0)
+    {
+        error.style.display = 'block';
+    } else if(book.length >=1)
+    {
+        const displayBook = document.getElementById('display-book');
+    displayBook.textContent = '';
+    const serachResult = document.getElementById('total-result');
+    serachResult.textContent = '';
+    const searchDiv = document.createElement('div');
+    searchDiv.innerHTML = `
+         <h3 class="text-center">i have ${book.length} result</h3>
+    `;
+    serachResult.appendChild(searchDiv);
+    book.forEach( everyBook => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+          <div class="col">
+            <div class="card">
+                <img height="300" src="https://covers.openlibrary.org/b/id/${everyBook.cover_i}-M.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">bookTitle:-${everyBook.title}</h5>
+                    <p class="card-text">Author:-${everyBook.author_name}</p>
+                    <p class="card-text">publisher:-${everyBook.publisher}</p>
+                    <p class="card-text">first_publish_year:-${everyBook.first_publish_year}</p>
+                </div>
+            </div>
+        </div>
+        `;
+        displayBook.appendChild(div);
+    });  
+    }
+  
+}
+
+
